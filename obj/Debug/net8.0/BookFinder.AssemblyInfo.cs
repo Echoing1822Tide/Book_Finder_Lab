cdd1f325700f2e4dff42d605c01e5da5a912cf23
James Gill <echoing1822tide@outlook.com>
2025-12-02 08:24:02 -0700

    "Initial Branch"

--- obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookFinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b43855f09ad088d94e81eaa1fccbfb387a1133")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
